67ddba441c73c0be045fb74416d30ae463e8d33c
LucasRoig <lucas1.roig@gmail.com>
2021-06-09 08:16:14 +0200

    add get and delete chess db in repo

--- app/db/repositories/chess_db.py
+++ app/db/repositories/chess_db.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from asyncpg import Record
+
 from app.db.repositories.base import BaseRepository
 from app.models.domain.chess_database import ChessDb
 from app.models.domain.users import Auth0User
@@ -11,8 +15,28 @@ RETURNING id, name, user_id
 class ChessDbRepository(BaseRepository):
     async def create_db(self, *, name: str, user: Auth0User) -> ChessDb:
         row = await self._log_and_fetch_row(CREATE_DB, name, user.id)
-        return ChessDb(
-            name=row["name"],
-            id=row["id"],
-            user_id=row["user_id"],
-        )
+        return parse_record(record=row)
+
+    async def delete_db(self, *, db_id: int) -> bool:
+        delete_db = """
+        DELETE FROM chess_db WHERE id = $1
+        """
+        await self._log_and_fetch_row(delete_db, db_id)
+        return True
+
+    async def get_db_for_user(self, *, user_id: int) -> List[ChessDb]:
+        find_db = """
+        SELECT * FROM chess_db WHERE user_id = $1
+        """
+        rows = await self._log_and_fetch(find_db, user_id)
+        return [
+            parse_record(record=record)
+            for record in rows
+        ]
+
+def parse_record(*, record: Record) -> ChessDb:
+    return ChessDb(
+        name=record["name"],
+        id=record["id"],
+        user_id=record["user_id"],
+    )
--- tests/test_repositories/test_chess_db_repo.py
+++ tests/test_repositories/test_chess_db_repo.py
@@ -14,3 +14,28 @@ async def test_create_db(pool: Pool, test_auth0_user: Auth0User):
         assert db.name == db_name
         assert db.user_id == test_auth0_user.id
         assert db.id >= 0
+
+async def test_get_db(pool: Pool, test_auth0_user: Auth0User):
+    db1 = "db1"
+    db2 = "db2"
+    async with pool.acquire() as connection:
+        repo = ChessDbRepository(connection)
+        await repo.create_db(name=db1, user=test_auth0_user)
+        await repo.create_db(name=db2, user=test_auth0_user)
+        res = await repo.get_db_for_user(user_id=test_auth0_user.id)
+        assert len(res) == 2
+        assert res[0].name == db1
+        assert res[0].user_id == test_auth0_user.id
+        assert res[0].id > 0
+        assert res[1].name == db2
+        assert res[1].id > 0 and res[1].id != res[0].id
+        assert res[1].user_id == test_auth0_user.id
+
+async def test_delete_db(pool: Pool, test_auth0_user: Auth0User):
+    async with pool.acquire() as connection:
+        repo = ChessDbRepository(connection)
+        res = await repo.create_db(name="test", user=test_auth0_user)
+        assert len(await repo.get_db_for_user(user_id=test_auth0_user.id)) == 1
+        res = await repo.delete_db(db_id=res.id)
+        assert res == True
+        assert len(await repo.get_db_for_user(user_id=test_auth0_user.id)) == 0
